b4cd8fc4a4926f9a94856e5c866883f5be27b6ed
John Bellone <john.bellone.jr@gmail.com>
2014-07-03 17:30:28 -0400

    Turn tests green.

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -1,17 +1,27 @@
 require 'spec_helper'
 
 describe 'lockrun::default' do
-  let(:chef_run) { ChefSpec::Runner.new.converge(described_recipe) }
+  let(:chef_run) { ChefSpec::Runner.new }
 
-  it 'creates lock path' do
-    expect(chef_run).to_not create_directory(chef_run.node['lockrun']['lock_path'])
+  context 'directory[lock_path]' do
+    before do
+      chef_run.node.set['lockrun']['lock_path'] = '/tmp/lockrun'
+      chef_run.converge(described_recipe)
+    end
+
+    it { expect(chef_run).to create_directory('/tmp/lockrun') }
   end
 
-  it 'includes build-essential::default' do
-    expect(chef_run).to include_recipe('build-essential::default')
+  context 'cookbook_file[lockrun.c]' do
+    let(:cache_file) { File.join(Chef::Config[:file_cache_path], 'lockrun.c') }
+    before { chef_run.converge(described_recipe) }
+
+    it { expect(chef_run).to create_cookbook_file(cache_file) }
   end
 
-  it 'builds lockrun' do
-    expect(chef_run).to run_bash('build-lockrun')
+  context 'bash[build-lockrun]' do
+    before { chef_run.converge(described_recipe) }
+
+    it { expect(chef_run).to run_bash('build-lockrun') }
   end
 end
