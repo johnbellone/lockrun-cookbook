1fd9bbc73ce54eab64f73a388e2717b8805be2aa
John Bellone <jbellone@bloomberg.net>
2015-08-06 15:50:03 -0400

    Fixes rubocop linting check and matcher.

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -3,7 +3,7 @@ if defined?(ChefSpec)
     ChefSpec::Matchers::ResourceMatcher.new(:lockrun_cron, :create, name)
   end
 
-  def delete_lockrun_cron(resource_name)
+  def delete_lockrun_cron(name)
     ChefSpec::Matchers::ResourceMatcher.new(:lockrun_cron, :delete, name)
   end
 end
