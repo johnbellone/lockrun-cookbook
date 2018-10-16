5c9da66b2f361e43b75369c2ee32c41dd8579490
Jeff Byrnes <thejeffbyrnes@gmail.com>
2018-10-16 16:32:07 -0400

    Add acceptance test for lockrun::default

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -1,3 +1,6 @@
+describe command 'lockrun' do
+  it { should exist }
+end
 
 describe crontab('root').commands '/usr/bin/env lockrun --lockfile=/var/run/chef-client.lockrun -- chef-client' do
   its('minutes') { should cmp 0 }
